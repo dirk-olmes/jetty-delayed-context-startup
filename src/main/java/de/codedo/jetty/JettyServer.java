851195523469610712fa5624a0bbced647800bdb
Dirk Olmes <dirk.olmes@codedo.de>
2019-10-07 14:26:34 +0200

    Register a ContextHandler that answers 503 and start the server right away. Do the time-consuming startup of the real app context from a separate thread and swap out handlers once the app context is ready to serve requests.

--- src/main/java/de/codedo/jetty/JettyServer.java
+++ src/main/java/de/codedo/jetty/JettyServer.java
@@ -2,34 +2,67 @@ package de.codedo.jetty;
 
 import java.io.File;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.resource.PathResource;
 import org.eclipse.jetty.util.resource.Resource;
 
 public class JettyServer
 {
+	private static final Logger LOG = Log.getLogger(JettyServer.class);
+
 	public static void main(String[] args) throws Exception
 	{
 		Server server = new Server(8181);
 
-		Resource baseResource = new PathResource(new File("/tmp"));
-
-		ServletContextHandler theRealContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		theRealContext.setContextPath("/");
-		theRealContext.setBaseResource(baseResource);
-		// server.setHandler(theRealContext);
-
-		theRealContext.addServlet(HelloServlet.class, "/hello");
-		theRealContext.addEventListener(new AppInitializer());
-
 		ContextHandler duringStartup = new DuringStartupHandler();
-		HandlerList handlerList = new HandlerList(duringStartup, theRealContext);
-		server.setHandler(handlerList);
+		HandlerList list = new HandlerList(duringStartup);
+		server.setHandler(list);
 
+		LOG.info("starting the server");
 		server.start();
+		LOG.info("done starting the server");
+
+		new Thread(() -> deployTheApp(server), "deploy").start();
+
 		server.join();
+	}
+
+	private static void deployTheApp(Server server)
+	{
+		try
+		{
+			LOG.info("deploying the real application context");
+
+			Resource baseResource = new PathResource(new File("/tmp"));
+
+			ServletContextHandler theRealContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+			theRealContext.setContextPath("/");
+			theRealContext.setBaseResource(baseResource);
+
+			theRealContext.addServlet(HelloServlet.class, "/hello");
+			theRealContext.addEventListener(new AppInitializer());
+
+			HandlerList list = (HandlerList)server.getHandler();
+			Handler duringStartupHandler = list.getHandlers()[0];
+
+			list.addHandler(theRealContext);
+			LOG.info("starting the real application context");
+			theRealContext.start();
+
+			LOG.info("removing the temporary handler");
+			list.removeHandler(duringStartupHandler);
+
+			LOG.info("done deploying the real application context");
+		}
+		catch (Exception ex)
+		{
+			LOG.warn("Exception during deferred deploy", ex);
+		}
 	}
 }
